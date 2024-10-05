3967d3e53cd8e28fa1223ad145861d7271e3ce9f
Shubham Kumar <103112458+Shubh0605@users.noreply.github.com>
2024-10-05 19:19:15 +0000

    commit

---
+++ src/main/ZOPSMART/PreorderTraversalOfTree.java
@@ -0,0 +1,5 @@
+Class PretorderTraversalOfTree{
+  public static void main(String[] args){
+    
+  }
+}
--- src/main/java/Array/Merge Intervals.java
+++ src/main/java/Array/Merge Intervals.java
@@ -1,6 +1,28 @@
+mport java.util.*;
 class MergeIntervals{
   public static void main(String[] args){
-    int [][] arr = {{1,3},{2,6},{8,10},{15,18}};
-    
+    int [] [] a = {{1,3},
+      {2,6},
+      {8,9},
+      {9,11},
+      {8,10},
+      {2,4},
+      {15,18},
+      {16,17}};
+    Arrays.sort(a,(o1,o1) -> Integers.compare(o1[0],o1[0]));//lamda function to sort 2d array
+    for(int i=0;i<a.length;i++){
+      
+    ArrayList<int []> ans = new ArrayList<>();
+    if(ans.isEmpty() || a[i][0] > ans.get(ans.size()-1)[1]){
+      ans.add(new int[]{a[i][0],a[i][1]});
+    }
+    else{
+      ans.get(ans.size()-1)[1] = Math.max(ans.get(ans.size()-1)[1],a[i][1]);
+    }
+  }
+    int [][] result = ans.toArray(new int[ans.size()][]);
+    for(int i : result){
+      System.out.print(Arrays.toString(i));
+    }
   }
 }
